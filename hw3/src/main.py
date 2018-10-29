f7cc334bde045fce792d906829c2aa53baf5a98c
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-10-29 03:37:06 -0500

    hw3

--- hw3/src/main.py
+++ hw3/src/main.py
@@ -1,28 +1,16 @@
-"""Recommender Systems."""
+"""Simple Recommender Systems."""
+
+from scipy import spatial
+from collections import defaultdict
 
 import sys
 import math
-import collections
 import numpy as np
 
 
 def read_input():
     """
     Read input from stdin.
-
-    Sample Inputs:
-        5 5
-        1 1 3.0
-        1 2 4.0
-        1 3 3.0
-        2 4 2.0
-        2 5 5.0
-        1 batman robin superhero
-        2 batman dark knight
-        3 dark knight returns
-        4 batman joker gotham
-        5 batman superhero
-        1 4
 
     The first line of the input contains 2 space seperated integers R M.
         R is the number of lines of rating information.
@@ -36,219 +24,146 @@ def read_input():
         for which you need to estimate the rating.
     """
     first_line = sys.stdin.readline().rstrip("\n").split(" ")
-    # number of rating information
-    R = int(first_line[0])
 
-    # total number of movies
-    global M
-    M = int(first_line[1])
+    # number of rating information and total number of movies
+    global R, M
+    R, M = int(first_line[0]), int(first_line[1])
 
     user_ids = []
     movie_ids = []
     ratings = []
 
-    # R(m) is the set of users that have rated the movie m in the available dataset.
-    Rm = {}
+    # R(m) is the set of users that have rated the movie m in the available dataset. Rm[movie] = list of users
+    Rm = defaultdict(list)
 
-    # R(u) are the set of the movies that have been rated by the user u.
-    Ru = {}
+    # R(u) are the set of the movies that have been rated by the user u.  Ru[user] = list of movies
+    Ru = defaultdict(list)
 
-    # user -> movie relation
+    # (user, movie) -> rating
     rating_dict = {}
+    user_movie_mapping = defaultdict(list)
+    user_ratings = defaultdict(list)
+    movie_ratings = defaultdict(list)
 
     for _ in range(R):
         rating_info = sys.stdin.readline().rstrip("\n").split(" ")
-        user_id = int(rating_info[0])
-        movie_id = int(rating_info[1])
-        rating = float(rating_info[2])
+        user_id, movie_id, rating = int(rating_info[0]), int(rating_info[1]), float(rating_info[2])
 
-        rating_dict["{},{}".format(user_id, movie_id)] = rating
+        rating_dict[(user_id, movie_id)] = rating
+        user_movie_mapping[user_id].append(movie_id)
+        movie_ratings[movie_id].append(rating)
+        user_ratings[user_id].append(rating)
 
         # mapping from movie to the users rated this movie
-        if movie_id not in Rm:
-            Rm[movie_id] = [user_id]
-        else:
-            Rm[movie_id].append(user_id)
+        Rm[movie_id].append(user_id)
 
         # mapping from user to the movies this user rated
-        if user_id not in Ru:
-            Ru[user_id] = [movie_id]
-        else:
-            Ru[user_id].append(movie_id)
+        Ru[user_id].append(movie_id)
 
-        movie_ids.append(movie_id)
+        # user_ids, movie_ids, ratings
         user_ids.append(user_id)
+        movie_ids.append(movie_id)
         ratings.append(rating)
 
-    movie_ids = list(set(movie_ids))
-    user_ids = list(set(user_ids))
-
     # µ is the global mean calculated across all the ratings available
+    global mu
     mu = sum(ratings) / len(ratings)
 
-    movies = []
+    # get all the movies' words
+    movie_meta_info = {}
+    word_movie_mapping = defaultdict(set)
     for _ in range(M):
         meta_info = sys.stdin.readline().rstrip("\n").split(" ")
-        movie_id = int(meta_info[0])
-        movie_name = " ".join(meta_info[1:])
-        movies.append(movie_name)
+        word_dict = defaultdict(int)
+        for word in meta_info[1:]:
+            word_movie_mapping[word].add(int(meta_info[0]))
+            word_dict[word] += 1
+        movie_meta_info[int(meta_info[0])] = dict(word_dict)
+
+    term_index = {}
+    words = list(word_movie_mapping.keys())
+    global V
+    V = len(word_movie_mapping)
+    for idx, word in enumerate(words):
+        term_index[word] = idx
 
     last_line = sys.stdin.readline().rstrip("\n").split(" ")
-    target_user_id = int(last_line[0])
-    target_movie_id = int(last_line[1])
+    global target_user_id, target_movie_id
+    target_user_id, target_movie_id = int(last_line[0]), int(last_line[1])
 
-    return target_user_id, target_movie_id, user_ids, movie_ids, ratings, rating_dict, Rm, Ru, mu, movies
-
-
-def tf(t, m):
-    """
-    Term Frequency.
-
-    tf(t,m) = Number of times term t appears in metadata of a movie m / Total number of terms in the metadata about m
-
-    set_movie_names[v_] string
-    movies[m_] list of strings
-    """
-    counter_t = collections.Counter(m)
-    return counter_t[t] / len(m)
-
-
-def idf(t, ms):
-    """
-    Inverse Document Frequency.
-
-    idf(t) = ln(Total number of movies in dataset / Number of movies with term t in it)
-
-    """
-    counter = 0
-    # print(ms)
-    # print(t)
-    for name in ms:
-        if t in name:
-            counter += 1
-
-    return math.log(M / counter)
-
-
-def calculate_similarity(d_m, d_j):
-    """
-    Cosine Similarity Calculation.
-
-    Args:
-        d_m:
-        d_j:
-
-    Returns:
-        s_mj: the similarity value between 2 movies m and j.
-    """
-    return d_m.dot(d_j.T)
+    return rating_dict, Rm, Ru, movie_ratings, user_ratings, movie_meta_info, word_movie_mapping, term_index, user_movie_mapping, movie_ratings
 
 
 def main():
-    """Main pipeline for Recommender Systems."""
-    target_user_id, target_movie_id, user_id, movie_id, rating, rating_dict, Rm, Ru, mu, movies = read_input()
-    U = len(user_id)
+    """Main pipeline for Simple Recommender Systems."""
+    rating_dict, Rm, Ru, movie_ratings, user_ratings, movie_meta_info, word_movie_mapping, term_index, user_movie_mapping, movie_ratings = read_input()
 
-    # rating matrix row -> users, column -> movies
-    rating_matrix = np.ones((U + 1, M + 1)) * -1
-    for key, val in rating_dict.items():
-        key_array = key.split(",")
-        u = int(key_array[0])
-        m = int(key_array[1])
-        rating_matrix[u][m] = val
+    # get target user rated movies
+    rated_target_movies = Ru[target_user_id]
 
-    print("rating_matrix")
-    print(rating_matrix)
-    print("=======================")
+    bm_mapping = defaultdict(float)
+    b_um = cal_b_m(movie_ratings, rating_dict, user_ratings, bm_mapping, user_movie_mapping, target_movie_id, target_user_id)
 
-    if rating_matrix[target_user_id][target_movie_id] != -1:
-        return rating_matrix[target_user_id][target_movie_id]
+    # calculate s_mj
+    s_mj = []
+    d_m = cal_dv(movie_meta_info, word_movie_mapping, term_index, target_movie_id)
+    for j in rated_target_movies:
+        d_j = cal_dv(movie_meta_info, word_movie_mapping, term_index, j)
+        s_mj.append(1 - spatial.distance.cosine(d_m, d_j))
 
-    # For the (u, m) pairs not present in the data, we will use the formula below
+    up = []
+    for j in range(len(rated_target_movies)):
+        r_uj = rating_dict[(target_user_id, rated_target_movies[j])]
+        b_uj = cal_b_m(movie_ratings, rating_dict, user_ratings, bm_mapping, user_movie_mapping, rated_target_movies[j], target_user_id)
+        up.append(s_mj[j] * (r_uj - b_uj))
 
-    b_m = {}
-    for u_ in range(1, rating_matrix.shape[0]):
-        for m_ in range(1, rating_matrix.shape[1]):
-            # if ``rating_matrix[u_][m_] == -1`` means no ratings for this (u, m) pair
-            if rating_matrix[u_][m_] == -1:
+    result = np.sum(up) / np.sum(s_mj)
+    print(round(b_um + result, 1))
 
-            # Equation 3.
-            # |R(m)|: how many users have rated this movie
-                len_rm = len(Rm[m_])
-                r_um = np.asscalar(rating_matrix.sum(axis=0)[m_])
-                b_m[m_] = (r_um - len_rm * mu) / len_rm
 
-    print("bm")
-    print(b_m)
-    print("=======================")
-    
-    b_u = {}
-    for u_ in range(1, rating_matrix.shape[0]):
-        for m_ in range(1, rating_matrix.shape[1]):
-            # Equation 4.
-            # |R(u)|: how many movies this user rated
-            if rating_matrix[u_][m_] == -1:
-                len_ru = len(Ru[u_])
-                r_um = np.asscalar(rating_matrix.sum(axis=1)[u_])
-                b_m_sum = 0
-                for movie in Ru[u_]:
-                    if movie in b_m:
-                        b_m_sum += b_m[movie]
-                    else:
-                        b_m[movie] = np.sum(rating_matrix[:,m_][rating_matrix[:,m_] != -1] - mu)
-                        b_m_sum += b_m[movie]
+def cal_dv(movie_meta_info, word_movie_mapping, term_index, m_id):
+    """Calculate d^m."""
+    tf_vector = np.zeros(V)
+    idf_vector = np.zeros(V)
 
-                b_u[u_] = (r_um - len_ru * mu - b_m_sum) / len_ru
+    words = movie_meta_info[m_id].keys()
+    total = sum(movie_meta_info[m_id].values())
 
-    print("bu")
-    print(b_u)
-    print("=======================")  
-                
-    # Equation 2.
-    b_um = np.zeros((U + 1, M + 1))
-    for u_ in range(1, b_um.shape[0]):
-        for m_ in range(1, b_um.shape[1]):
-            b_um[u_][m_] = mu + b_u[u_] + b_m[m_]
+    for word in words:
+        # tf
+        tf_vector[term_index[word]] = movie_meta_info[m_id][word] / total
+        # idf
+        idf_vector[term_index[word]] = math.log(M / len(word_movie_mapping[word]))
+    dm = tf_vector * idf_vector
+    return dm.tolist()
 
-    # corpus -> movies list of movie names
-    movie_names = ""
-    for movie in movies:
-        movie_names += (movie + " ")
 
-    set_movie_names = list(set(movie_names.rstrip().split(" ")))
-    matrix_movie_names = np.zeros((M + 1, len(set_movie_names)))
-    for m_ in range(1, matrix_movie_names.shape[0]):
-        for v_ in range(matrix_movie_names.shape[1]):
-            matrix_movie_names[m_][v_] = tf(set_movie_names[v_], movies[m_ - 1].split(" ")) * idf(set_movie_names[v_ - 1], movies)
+def cal_b(movie_ratings, bm_mapping, m_id):
+    m_id_rates = movie_ratings[m_id]
+    val = np.sum(np.array(m_id_rates) - mu) / len(m_id_rates)
+    bm_mapping[m_id] = val
+    return val
 
-    print(set_movie_names)
-    print(matrix_movie_names[1])
-    print("+++")
-    print(matrix_movie_names)
-    print("=======================")  
-    
-    # Equation 1.
-    result = b_um[target_user_id][target_movie_id]
-    s_mj = np.zeros((M + 1, M + 1))
-    up = 0.
-    for j in Ru[target_user_id]:
-        s_mj[target_movie_id][j] = calculate_similarity(matrix_movie_names[target_movie_id], matrix_movie_names[j])
 
-    print(s_mj)
-    print("=======================")  
-        
-    for j in Ru[target_user_id]:
-        r_uj = rating_matrix[target_user_id][j]
-        b_uj = b_um[target_user_id][j]
-        up += s_mj[target_movie_id][j] * (r_uj - b_uj)
+def cal_b_m(movie_ratings, rating_dict, user_ratings, bm_mapping, user_movie_mapping, m_id, u_id):
+    """Calculate b_m."""
+    if m_id in bm_mapping:
+        b_m = bm_mapping[m_id]
+    else:
+        b_m = cal_b(movie_ratings, bm_mapping, m_id)
 
-    down = 0.
-    for j in Ru[target_user_id]:
-        down += s_mj[target_movie_id][j]
-    print(result)
-    print(up)
-    print(down)
-    print(round(result * up / down, 1))
+    movie_lists = user_movie_mapping[u_id]
+    up = []
+    up = 0
+    for m_id in movie_lists:
+        if m_id in bm_mapping:
+            val = bm_mapping[m_id]
+        else:
+            val = cal_b(movie_ratings, bm_mapping, m_id)
+        up += rating_dict[(u_id, m_id)] - mu - val
+    b_u = up / len(user_ratings[u_id])
+    return mu + b_u + b_m
+
 
 if __name__ == '__main__':
     main()
