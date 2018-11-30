313c33e7cc11cbf616d84334bb971cc754f5cf52
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-11-30 17:41:12 -0600

    update hw3

--- hw3/src/main.py
+++ hw3/src/main.py
@@ -8,6 +8,36 @@ import math
 import numpy as np
 
 
+def main():
+    """Main pipeline for Simple Recommender Systems."""
+    rating_dict, Rm, Ru, movie_ratings, user_ratings, movie_meta_info, word_movie_mapping, term_index, user_movie_mapping, movie_ratings = read_input()
+
+    # get target user rated movies
+    rated_target_movies = Ru[target_user_id]
+
+    bm_mapping = defaultdict(float)
+    b_um = cal_b_m(movie_ratings, rating_dict, user_ratings, bm_mapping, user_movie_mapping, target_movie_id, target_user_id)
+
+    # calculate s_mj
+    s_mj = []
+    d_m = cal_dv(movie_meta_info, word_movie_mapping, term_index, target_movie_id)
+    for j in rated_target_movies:
+        d_j = cal_dv(movie_meta_info, word_movie_mapping, term_index, j)
+        s_mj.append(1 - spatial.distance.cosine(d_m, d_j))
+
+    up = []
+    for j in range(len(rated_target_movies)):
+        r_uj = rating_dict[(target_user_id, rated_target_movies[j])]
+        b_uj = cal_b_m(movie_ratings, rating_dict, user_ratings, bm_mapping, user_movie_mapping, rated_target_movies[j], target_user_id)
+        up.append(s_mj[j] * (r_uj - b_uj))
+
+    result = np.sum(up) / np.sum(s_mj)
+    result += b_um
+
+    # print out
+    print(round(result, 1))
+
+
 def read_input():
     """
     Read input from stdin.
@@ -94,51 +124,29 @@ def read_input():
     return rating_dict, Rm, Ru, movie_ratings, user_ratings, movie_meta_info, word_movie_mapping, term_index, user_movie_mapping, movie_ratings
 
 
-def main():
-    """Main pipeline for Simple Recommender Systems."""
-    rating_dict, Rm, Ru, movie_ratings, user_ratings, movie_meta_info, word_movie_mapping, term_index, user_movie_mapping, movie_ratings = read_input()
-
-    # get target user rated movies
-    rated_target_movies = Ru[target_user_id]
-
-    bm_mapping = defaultdict(float)
-    b_um = cal_b_m(movie_ratings, rating_dict, user_ratings, bm_mapping, user_movie_mapping, target_movie_id, target_user_id)
-
-    # calculate s_mj
-    s_mj = []
-    d_m = cal_dv(movie_meta_info, word_movie_mapping, term_index, target_movie_id)
-    for j in rated_target_movies:
-        d_j = cal_dv(movie_meta_info, word_movie_mapping, term_index, j)
-        s_mj.append(1 - spatial.distance.cosine(d_m, d_j))
-
-    up = []
-    for j in range(len(rated_target_movies)):
-        r_uj = rating_dict[(target_user_id, rated_target_movies[j])]
-        b_uj = cal_b_m(movie_ratings, rating_dict, user_ratings, bm_mapping, user_movie_mapping, rated_target_movies[j], target_user_id)
-        up.append(s_mj[j] * (r_uj - b_uj))
-
-    result = np.sum(up) / np.sum(s_mj)
-    print(round(b_um + result, 1))
-
-
 def cal_dv(movie_meta_info, word_movie_mapping, term_index, m_id):
-    """Calculate d^m."""
-    tf_vector = np.zeros(V)
-    idf_vector = np.zeros(V)
+    """
+    Convert metadata document for movie m into |V| dimensional vector d^m.
+
+    together with tf-idf calculation
+    """
+    tf_vector, idf_vector = np.zeros(V), np.zeros(V)
 
     words = movie_meta_info[m_id].keys()
     total = sum(movie_meta_info[m_id].values())
 
     for word in words:
-        # tf
+        # tf vector
         tf_vector[term_index[word]] = movie_meta_info[m_id][word] / total
-        # idf
+        # idf vector
         idf_vector[term_index[word]] = math.log(M / len(word_movie_mapping[word]))
-    dm = tf_vector * idf_vector
-    return dm.tolist()
+
+    d_m = tf_vector * idf_vector
+    return d_m.tolist()
 
 
 def cal_b(movie_ratings, bm_mapping, m_id):
+    """Equation 4."""
     m_id_rates = movie_ratings[m_id]
     val = np.sum(np.array(m_id_rates) - mu) / len(m_id_rates)
     bm_mapping[m_id] = val
@@ -146,7 +154,11 @@ def cal_b(movie_ratings, bm_mapping, m_id):
 
 
 def cal_b_m(movie_ratings, rating_dict, user_ratings, bm_mapping, user_movie_mapping, m_id, u_id):
-    """Calculate b_m."""
+    """
+    Calculate b_m.
+
+    Equation 3.
+    """
     if m_id in bm_mapping:
         b_m = bm_mapping[m_id]
     else:
